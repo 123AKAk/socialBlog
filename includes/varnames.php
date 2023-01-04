8026332e1f8e42d92ffac5b410adf766d8ca38e3
E_A <46349404+123AKAk@users.noreply.github.com>
2023-01-04 16:00:06 +0100

    edited shared components and mail functions

--- includes/varnames.php
+++ includes/varnames.php
@@ -1,3 +1,15 @@
-<?php
-
+<?php 
+    $sitename = "BLUNT";
+    $globalname = "BLUNT";
+    $siteemail = "admin@bluntechnology.com";
+    $siteemailpassword = "5a51EZbK9jKj";
+    $siteemailhost = "mail.bluntechnology.com";
+    $siteemailport = 465;
+    $sitemsg = "Check out this Post on ";
+    $sitehashtag = "BLUNT BLOGGING NIGERIA";
+    $pagetitle = "BLUNT BLOG";
+    $pagedesc = "BLOGGING FOR EVERYONE";
+    $siteurl = "http://bluntechnology.com/";
+    $pagelogo = "http://bluntechnology.com/assets/img/logo.png";
+    $logodesc = "Nice Color";
 ?>
