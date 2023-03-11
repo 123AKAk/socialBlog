7d28b095a9039529aee1c9eea0e3d6d580804a62
E_A <46349404+123AKAk@users.noreply.github.com>
2023-03-11 10:28:10 +0100

    code js

--- scripts/code.js
+++ scripts/code.js
@@ -15,53 +15,59 @@ window.onload = getLocation;
         {
             alertify.error("Fill all Input Feilds");
         }
-        if(ValidateEmail(email) == false)
-        {
-            alertify.error("Invalid Email, use a Valid Email");
-        }
-        if(password != confrimpassword)
-        {
-            alertify.error("Passwords are not the same");
-        }
         else
         {
-            if($("#agreed").is(':checked'))
+            if(ValidateEmail(email) == false)
             {
-                let formdata = new FormData();
-                formdata.append("username", username);
-                formdata.append("email", username);
-                formdata.append("gender", gender);
-                formdata.append("password", password);
-                formdata.append("user_ipaddress", ipaddress);
-                formdata.append("user_country", country);
-
-                let loca = "classes/components/userComponents.php?dataPurpose=signup";
-                fetch(loca, { method: "POST", body: formdata })
-                .then(res => res.text())
-                .then(data => 
-                {
-                    var result = JSON.parse(data);
-                    if(result.response == true)
-                    {
-                        alertify.success(result.message);
-                        alertify.message('Redirecting...');
-                        setTimeout(function(){
-                            window.location.replace("login.php");
-                        }, 1000);
-
-                        // alertify.alert(result.message, function(){
-                        //     alertify.message('OK');
-                        // });
-                    }
-                    else
-                    {
-                        alertify.error(result.message);
-                    }
-                });
+                alertify.error("Invalid Email, use a Valid Email");
             }
             else
             {
-                alertify.error("Accpet Terms and Agreement to continue");   
+                if(password != confrimpassword && password == "")
+                {
+                    alertify.error("Passwords are not the same");
+                }
+                else
+                {
+                    if($("#agreed").is(':checked'))
+                    {
+                        let formdata = new FormData();
+                        formdata.append("username", username);
+                        formdata.append("email", username);
+                        formdata.append("gender", gender);
+                        formdata.append("password", password);
+                        formdata.append("user_ipaddress", ipaddress);
+                        formdata.append("user_country", country);
+        
+                        let loca = "classes/components/userComponents.php?dataPurpose=signup";
+                        fetch(loca, { method: "POST", body: formdata })
+                        .then(res => res.text())
+                        .then(data => 
+                        {
+                            var result = JSON.parse(data);
+                            if(result.response == true)
+                            {
+                                alertify.success(result.message);
+                                alertify.message('Redirecting...');
+                                setTimeout(function(){
+                                    window.location.replace("login.php");
+                                }, 1000);
+        
+                                // alertify.alert(result.message, function(){
+                                //     alertify.message('OK');
+                                // });
+                            }
+                            else
+                            {
+                                alertify.error(result.message);
+                            }
+                        });
+                    }
+                    else
+                    {
+                        alertify.error("Accpet Terms and Agreement to continue");   
+                    }
+                }
             }
         }
 
