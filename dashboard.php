f1dcafd051aa7190910b53198ca2f23c8c555a35
E_A <46349404+123AKAk@users.noreply.github.com>
2023-03-24 14:39:00 +0100

    Omoh

--- dashboard.php
+++ dashboard.php
@@ -968,7 +968,7 @@ include 'includes/scripts.php';
                 method: "POST",
                 body: formdata,
             })
-            .then(res => res.json())
+            .then(res => res.text())
             .then(data => {
                 console.log(data)
                 //var post_contentsVal = data.postContents;
